02efcf56b46f5cf14f928a5a05c836ccb8408ac7
Alois Janíček <janicek.dev@gmail.com>
2019-05-02 23:24:58 +0200

    use newly available flag for icons in ivy

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
        ;; ido               ; the other *other* search engine...
        (ivy              ; a search engine for love and life
         ;; +fuzzy           ; enable fuzzy search backend for ivy
+        +icons
         +childframe)
 
        :ui
