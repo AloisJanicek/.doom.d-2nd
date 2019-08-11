b70c3da5644ba794461173266942490059fe3a76
Alois Janíček <janicek.dev@gmail.com>
2019-08-11 11:53:23 +0200

    I probably don't need this

--- config.el
+++ config.el
@@ -101,7 +101,7 @@ should be agenda-view filtered by `org-agenda-tag-filter-preset'.")
 
 (load! "+bindings")
 
-(add-hook 'org-load-hook '(lambda () (setq org-modules (append '(org-man org-eww org-protocol org-habit) org-modules))))
+;; (add-hook 'org-load-hook '(lambda () (setq org-modules (append '(org-man org-eww org-protocol org-habit) org-modules))))
 
 (set-popup-rule! "*backtrace\*" :size 0.4 :side 'right :select t)
 (set-popup-rule! "^ \\*company-box-" :ignore t)
