b1372570e22dde1a0cde785a83d6544760ab16ca
Alois Janíček <janicek.dev@gmail.com>
2019-08-29 09:44:38 +0200

    fix indent

--- +bindings.el
+++ +bindings.el
@@ -30,7 +30,7 @@
  "M-F"       #'swiper-all
  "M-s"       #'save-buffer
  "C-s"       #'ispell-word
-:in "C-="       #'recenter-top-bottom
+ :in "C-="       #'recenter-top-bottom
  "C-\\"      #'move-to-window-line-top-bottom
  "C-<right>" #'next-buffer
  "C-<left>"  #'previous-buffer
