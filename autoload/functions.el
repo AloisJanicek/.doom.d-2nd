f5bbc49e5c001ffd80a7c9fefb3a20415d950c3a
Alois Janíček <janicek.dev@gmail.com>
2020-03-03 00:20:53 +0100

    remove comments with TODO keywords
    This isn't my problem, I just temporarily copied it

--- autoload/functions.el
+++ autoload/functions.el
@@ -836,36 +836,16 @@ return an empty string."
   (if (not element)
       ""
     (let* ((properties (cadr element))
-           ;; Remove the :parent property, which so bloats the size of
-           ;; the properties list that it makes it essentially
-           ;; impossible to debug, because Emacs takes approximately
-           ;; forever to show it in the minibuffer or with
-           ;; `describe-text-properties'.  FIXME: Shouldn't be necessary
-           ;; anymore since we're not parsing the whole buffer.
-
-           ;; Also, remove ":" from key symbols.  FIXME: It would be
-           ;; better to avoid this somehow.  At least, we should use a
-           ;; function to convert plists to alists, if possible.
            (properties (cl-loop for (key val) on properties by #'cddr
                                 for symbol = (intern (cl-subseq (symbol-name key) 1))
                                 unless (member symbol '(parent))
                                 append (list symbol val)))
-           ;; TODO: --add-faces is used to add the :relative-due-date property, but that fact is
-           ;; hidden by doing it through --add-faces (which calls --add-scheduled-face and
-           ;; --add-deadline-face), and doing it in this form that gets the title hides it even more.
-           ;; Adding the relative due date property should probably be done explicitly and separately
-           ;; (which would also make it easier to do it independently of faces, etc).
            (title (--> (org-ql-view--add-faces element)
                        (org-element-property :raw-value it)
                        (org-link-display-format it)))
            (todo-keyword (-some--> (org-element-property :todo-keyword element)
                                    (org-ql-view--add-todo-face it)))
-           ;; FIXME: Figure out whether I should use `org-agenda-use-tag-inheritance' or `org-use-tag-inheritance', etc.
            (tag-list (if org-use-tag-inheritance
-                         ;; FIXME: Note that tag inheritance cannot be used here unless markers are
-                         ;; added, otherwise we can't go to the item's buffer to look for inherited
-                         ;; tags.  (Or does `org-element-headline-parser' parse inherited tags too?  I
-                         ;; forget...)
                          (if-let ((marker (or (org-element-property :org-hd-marker element)
                                               (org-element-property :org-marker element))))
                              (with-current-buffer (marker-buffer marker)
@@ -896,7 +876,6 @@ return an empty string."
       (remove-list-of-text-properties 0 (length string) '(line-prefix) string)
       ;; Add all the necessary properties and faces to the whole string
       (--> string
-           ;; FIXME: Use proper prefix
            (concat
             (if effort
                 effort
