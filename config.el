4a3c3b0135aaa5af199dd283f56d4619b861f0dd
Alois Janíček <janicek.dev@gmail.com>
2020-03-09 13:07:56 +0100

    move things around
    after! blocks first, then use-package!

--- config.el
+++ config.el
@@ -8,7 +8,6 @@ if running under WSL")
                      (concat "/mnt/c/Users/" (aj/return-wsl-user-name) "/")
                    (setq +BASE-HOME (expand-file-name "~/"))))
 
-
 (defvar +Reference (concat +BASE-HOME "Documents/MEGAsync")
   "Location of Reference folder.")
 
@@ -84,150 +83,11 @@ if running under WSL")
 (set-popup-rule! "*backtrace\*"                  :size 0.5  :side 'bottom :select t)
 (set-popup-rule! "^ \\*company-box-" :ignore t)
 
-(use-package! ace-link)
-
-(use-package! ahk-mode
-  :commands ahk-mode
-  )
-
-(use-package! anki-editor
-  :commands anki-editor-mode
-  :config
-  (setq anki-editor-create-decks t)
-  )
-
-(use-package! apache-mode
-  :commands apache-mode
-  )
-
-(use-package! company-posframe
-  :after company
-  :config
-  (company-posframe-mode 1)
-  (setq company-posframe-quickhelp-delay nil
-        company-posframe-show-indicator nil
-        company-posframe-show-metadata nil
-        )
-  )
-
-(use-package! counsel-org-clock
-  :commands (counsel-org-clock-context
-             counsel-org-clock-history
-             counsel-org-clock-goto
-             )
-  :config
-  (setq counsel-org-clock-history-limit 20)
-  )
-
-(use-package! counsel-tramp
-  :commands counsel-tramp
-  )
-
-(use-package! counsel-web
-  :commands (counsel-web-search counsel-web-suggest)
-  :config
-  (setq
-   counsel-web-search-function #'counsel-web-search--google
-   counsel-web-search-action #'browse-url-default-browser
-   counsel-web-search-alternate-action #'eww
-   counsel-web-search-dynamic-update t
-   )
-  )
-
-(use-package! define-word
-  :commands (define-word  define-word-at-point))
-
-(use-package! ereader
-  :commands (ereader-read-epub ereader-mode)
-  :mode ("\\.epub\\'". ereader-mode)
-  :config
-  (add-hook 'ereader-mode-hook #'hide-mode-line-mode)
-  (add-hook 'ereader-mode-hook #'visual-line-mode)
-  (add-hook 'ereader-mode-hook #'turn-off-solaire-mode)
-  )
-
-(use-package! esqlite
-  :commands (esqlite-stream-open esqlite-read))
-
-(use-package! google-translate
-  :commands (google-translate-at-point
-             google-translate-at-point-reverse)
-  :config
-  (setq google-translate-default-source-language "cs"
-        google-translate-default-target-language "en")
-  (set-popup-rule! "*Google Translate\*"        :size 0.4  :side 'top :select t))
-
-(use-package! highlight-blocks
-  :commands (highlight-blocks-mode highlight-blocks-now))
-
-(use-package! hungry-delete
-  :demand t
-  :config
-  (setq hungry-delete-except-modes
-        '(term-mode help-mode minibuffer-inactive-mode calc-mode))
-  (global-hungry-delete-mode 1))
-
-(use-package! howdoyou
-  :commands (howdoyou-query aj/howdoyou/body)
-  :config
-  (set-popup-rule! "*How Do You*"      :vslot 1 :size 0.4  :side 'left :select t :ttl nil)
-  )
-
-(use-package! indium
-  :commands indium-connect
-  )
-
-(use-package! imenu-list
-  :commands imenu-list-smart-toggle
-  :config
-  (set-popup-rule! "^\\*Ilist"
-    :side 'right :size 35 :select nil :ttl nil :quit nil)
-  ;; First create new face which is a copy of hl-line-face
-  (copy-face 'hl-line 'hl-line-imenu-list-face)
-  ;; Change what you want in this new face
-  (set-face-attribute 'hl-line-imenu-list-face nil
-                      :background `,(doom-color 'base4) :weight 'bold :underline t)
-
-  (set-face-attribute 'imenu-list-entry-face-0 nil
-                      :foreground `,(doom-color 'blue))
-  (set-face-attribute 'imenu-list-entry-face-1 nil
-                      :foreground `,(doom-color 'magenta))
-  (set-face-attribute 'imenu-list-entry-face-2 nil
-                      :foreground `,(doom-color 'violet))
-  (set-face-attribute 'imenu-list-entry-face-3 nil
-                      :foreground `,(doom-lighten 'blue 0.25))
-  ;; Finally, the hook
-  (add-hook 'imenu-list-major-mode-hook (lambda ()
-                                          (set (make-local-variable 'hl-line-face)
-                                               'hl-line-imenu-list-face)
-                                          (hl-line-mode)))
-  (add-hook 'imenu-list-major-mode-hook #'variable-pitch-mode)
-  )
-
-(use-package! ivy-yasnippet
-  :commands (ivy-yasnippet))
-
-;; for navigation in epub files
-(use-package! ivy-pages
-  :commands ivy-pages)
-
-(use-package! js-react-redux-yasnippets
-  )
-
-(use-package! link-hint
-  :commands (link-hint-open-all-links
-             link-hint-copy-all-links
-             link-hint-open-link
-             link-hint-copy-link)
-  :config
-  (setq link-hint-avy-all-windows nil)
-  )
-
-(use-package! ob-javascript
-  :after ob-core
-  :config
-  (advice-add #'ob-javascript--node-path :override #'aj/ob-javascript--node-path))
-
+(after! alert
+  (setq alert-default-style 'libnotify)
+  (setq alert-libnotify-command (if (aj/wsl-p)
+                                    (expand-file-name "notify-wsl" "~/.local/bin")
+                                  "/usr/bin/notify-send")))
 
 (after! all-the-icons-ivy
   (dolist (cmd '( counsel-find-file
@@ -238,137 +98,6 @@ if running under WSL")
                   counsel-projectile-switch-project))
     (ivy-set-display-transformer cmd #'all-the-icons-ivy-file-transformer)))
 
-(after! evil-snipe
-  (add-to-list 'evil-snipe-disabled-modes 'org-brain-visualize-mode nil #'eq)
-  )
-
-(after! epg
-  (setq epg-pinentry-mode 'ask))
-
-(after! geiser
-  (setq geiser-default-implementation 'guile))
-
-(use-package! org-brain
-  ;; :after org
-  :commands
-  (org-brain-visualize
-   org-brain-add-parent
-   org-brain-add-child
-   org-brain-add-friendship
-   org-brain-add-relationship
-   org-brain-add-resource
-   org-brain-goto-parent
-   org-brain-goto-child
-   org-brain-goto-friend
-   org-brain-goto-current
-   org-brain-goto-end
-   org-brain-goto-other-window
-   org-brain-remove-child
-   org-brain-remove-friendship
-   org-brain-remove-parent
-   )
-  :init
-  (add-to-list 'evil-motion-state-modes 'org-brain-visualize-mode)
-  :config
-  (add-hook 'org-brain-visualize-mode-hook #'visual-line-mode)
-  (advice-add #'org-brain-visualize :after #'aj/take-care-of-org-buffers)
-  (advice-add #'org-brain-entry-at-pt :override #'aj/org-brain-entry-at-pt)
-  (setq org-brain-visualize-default-choices 'all
-        org-brain-title-max-length -1
-        org-brain-path +TECHNICAL
-        org-brain-data-file (expand-file-name ".org-brain-data.el" (concat user-emacs-directory ".local/cache"))
-        org-brain-include-file-entries t
-        org-brain-file-entries-use-title t
-        )
-  )
-
-(use-package! org-ebook
-  :commands (org-ebook-open org-ebook-store-link)
-  )
-
-(use-package! org-pdfview
-  :commands (org-pdfview-open org-pdfview-store-link org-pdfview-complete-link org-pdfview-export)
-  )
-
-(after! org-pomodoro
-  (setq alert-user-configuration (quote ((((:category . "org-pomodoro")) libnotify nil)))
-        org-pomodoro-ask-upon-killing nil))
-
-(use-package! org-ql
-  :after org
-  :commands org-ql-search
-  :config
-  (advice-add #'org-ql--select :around #'doom-shut-up-a)
-  (advice-add #'org-ql-view-refresh :around #'doom-shut-up-a)
-  (advice-add #'org-ql-view-refresh :after (lambda (&rest _) (org-agenda-filter-apply aj/agenda-filter 'tag)))
-  (advice-add #'org-ql-view--format-element :override #'aj/org-ql-view--format-element)
-  )
-
-(use-package! org-sidebar
-  :commands (org-sidebar org-sidebar-tree org-sidebar-ql)
-  )
-
-(use-package! org-super-agenda
-  :after org
-  :config
-  (org-super-agenda-mode)
-  (setq org-super-agenda-header-map (make-sparse-keymap))
-  )
-
-(use-package! powerthesaurus
-  :commands (powerthesaurus-lookup-word
-             powerthesaurus-lookup-word-dwim
-             powerthesaurus-lookup-word-at-point))
-
-(use-package! robots-txt-mode
-  :mode (("/robots\\.txt\\'" . robots-txt-mode)))
-
-(use-package! sdcv
-  :commands (sdcv-search-input sdcv-search-pointer)
-  :config
-  (set-popup-rule! "*SDCV\*"                    :size 0.4  :side 'top :select t))
-
-(use-package! systemd
-  :commands (systemd-mode))
-
-;; needed for ereader
-(use-package! xml+
-  :commands (xml+-query--generic xml+-query-all xml+-query-first xml+-node-text xml+-node-text--helper))
-
-(use-package! x-path-walker
-  :commands (helm-x-path-walker  x-path-get-mode))
-
-(use-package! vimrc-mode
-  :commands vimrc-mode
-  )
-
-(use-package! yankpad
-  :commands
-  (yankpad-append-category
-   yankpad-capture-snippet
-   yankpad-edit
-   yankpad-expand
-   yankpad-insert
-   yankpad-map
-   yankpad-reload
-   yankpad-repeat
-   yankpad-set-category)
-  :config
-  (setq yankpad-file (expand-file-name "yankpad.org" org-directory))
-  )
-
-(use-package! zeal-at-point
-  :commands (zeal-at-point zeal-at-point-search zeal-at-point-set-docset)
-  :config
-  (add-to-list 'zeal-at-point-mode-alist '(web-mode . "html"))
-  (add-to-list 'zeal-at-point-mode-alist '(pug-mode . ("html" "pug")))
-  )
-
-(after! alert
-  (setq alert-default-style 'libnotify)
-  (setq alert-libnotify-command (if (aj/wsl-p)
-                                    (expand-file-name "notify-wsl" "~/.local/bin")
-                                  "/usr/bin/notify-send")))
 (after! ansible-doc
   (set-popup-rule! "*ansible-doc "     :vslot 2 :size 0.32 :side 'left :select t :ttl t)
   (add-hook 'ansible-doc-module-mode-hook #'evil-normal-state)
@@ -385,6 +114,7 @@ if running under WSL")
 (after! avy
   (setq avy-all-windows t
         avy-background t))
+
 (after! calendar
   (setq calendar-week-start-day 1))
 
@@ -439,6 +169,9 @@ if running under WSL")
             t)
   )
 
+(after! epg
+  (setq epg-pinentry-mode 'ask))
+
 (after! evil
   (setq evil-move-cursor-back nil))
 
@@ -448,6 +181,10 @@ if running under WSL")
 
 (after! evil-org-agenda
   (advice-add #'evil-org-agenda-set-keys :after #'aj/fix-evil-org-agenda-keys)
+  )
+
+(after! evil-snipe
+  (add-to-list 'evil-snipe-disabled-modes 'org-brain-visualize-mode nil #'eq)
   )
 
 (after! emmet-mode
@@ -494,6 +231,9 @@ if running under WSL")
   (setq flyspell-issue-message-flag nil
         flyspell-issue-welcome-flag nil)
   (add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC")))
+
+(after! geiser
+  (setq geiser-default-implementation 'guile))
 
 (after! helm
   (helm-mode -1))
@@ -620,13 +360,13 @@ if running under WSL")
 (after! org
   ;; autoload missing encrypt / decrypt commands
   (unless
-    (fboundp 'org-decrypt-entries)
+      (fboundp 'org-decrypt-entries)
     (autoload
       (function org-decrypt-entries)
       "org-crypt" nil t))
 
   (unless
-    (fboundp 'org-encrypt-entry)
+      (fboundp 'org-encrypt-entry)
     (autoload
       (function org-encrypt-entry)
       "org-crypt" nil t))
@@ -850,6 +590,10 @@ if running under WSL")
 ;; (after! org-protocol
 ;;   (load! "local/org-protocol-capture-html/org-protocol-capture-html.el"))
 
+(after! org-pomodoro
+  (setq alert-user-configuration (quote ((((:category . "org-pomodoro")) libnotify nil)))
+        org-pomodoro-ask-upon-killing nil))
+
 (after! pdf-view
   (setq pdf-view-midnight-colors `(,(doom-color 'fg) . ,(doom-color 'bg-alt)))
   (add-hook 'pdf-view-mode-hook (lambda ()
@@ -976,6 +720,262 @@ if running under WSL")
   (setq yas-wrap-around-region t
         yas-triggers-in-field t
         ))
+
+(use-package! ace-link)
+
+(use-package! ahk-mode
+  :commands ahk-mode
+  )
+
+(use-package! anki-editor
+  :commands anki-editor-mode
+  :config
+  (setq anki-editor-create-decks t)
+  )
+
+(use-package! apache-mode
+  :commands apache-mode
+  )
+
+(use-package! company-posframe
+  :after company
+  :config
+  (company-posframe-mode 1)
+  (setq company-posframe-quickhelp-delay nil
+        company-posframe-show-indicator nil
+        company-posframe-show-metadata nil
+        )
+  )
+
+(use-package! counsel-org-clock
+  :commands (counsel-org-clock-context
+             counsel-org-clock-history
+             counsel-org-clock-goto
+             )
+  :config
+  (setq counsel-org-clock-history-limit 20)
+  )
+
+(use-package! counsel-tramp
+  :commands counsel-tramp
+  )
+
+(use-package! counsel-web
+  :commands (counsel-web-search counsel-web-suggest)
+  :config
+  (setq
+   counsel-web-search-function #'counsel-web-search--google
+   counsel-web-search-action #'browse-url-default-browser
+   counsel-web-search-alternate-action #'eww
+   counsel-web-search-dynamic-update t
+   )
+  )
+
+(use-package! define-word
+  :commands (define-word  define-word-at-point))
+
+(use-package! ereader
+  :commands (ereader-read-epub ereader-mode)
+  :mode ("\\.epub\\'". ereader-mode)
+  :config
+  (add-hook 'ereader-mode-hook #'hide-mode-line-mode)
+  (add-hook 'ereader-mode-hook #'visual-line-mode)
+  (add-hook 'ereader-mode-hook #'turn-off-solaire-mode)
+  )
+
+(use-package! esqlite
+  :commands (esqlite-stream-open esqlite-read))
+
+(use-package! google-translate
+  :commands (google-translate-at-point
+             google-translate-at-point-reverse)
+  :config
+  (setq google-translate-default-source-language "cs"
+        google-translate-default-target-language "en")
+  (set-popup-rule! "*Google Translate\*"        :size 0.4  :side 'top :select t))
+
+(use-package! highlight-blocks
+  :commands (highlight-blocks-mode highlight-blocks-now))
+
+(use-package! hungry-delete
+  :demand t
+  :config
+  (setq hungry-delete-except-modes
+        '(term-mode help-mode minibuffer-inactive-mode calc-mode))
+  (global-hungry-delete-mode 1))
+
+(use-package! howdoyou
+  :commands (howdoyou-query aj/howdoyou/body)
+  :config
+  (set-popup-rule! "*How Do You*"      :vslot 1 :size 0.4  :side 'left :select t :ttl nil)
+  )
+
+(use-package! indium
+  :commands indium-connect
+  )
+
+(use-package! imenu-list
+  :commands imenu-list-smart-toggle
+  :config
+  (set-popup-rule! "^\\*Ilist"
+    :side 'right :size 35 :select nil :ttl nil :quit nil)
+  ;; First create new face which is a copy of hl-line-face
+  (copy-face 'hl-line 'hl-line-imenu-list-face)
+  ;; Change what you want in this new face
+  (set-face-attribute 'hl-line-imenu-list-face nil
+                      :background `,(doom-color 'base4) :weight 'bold :underline t)
+
+  (set-face-attribute 'imenu-list-entry-face-0 nil
+                      :foreground `,(doom-color 'blue))
+  (set-face-attribute 'imenu-list-entry-face-1 nil
+                      :foreground `,(doom-color 'magenta))
+  (set-face-attribute 'imenu-list-entry-face-2 nil
+                      :foreground `,(doom-color 'violet))
+  (set-face-attribute 'imenu-list-entry-face-3 nil
+                      :foreground `,(doom-lighten 'blue 0.25))
+  ;; Finally, the hook
+  (add-hook 'imenu-list-major-mode-hook (lambda ()
+                                          (set (make-local-variable 'hl-line-face)
+                                               'hl-line-imenu-list-face)
+                                          (hl-line-mode)))
+  (add-hook 'imenu-list-major-mode-hook #'variable-pitch-mode)
+  )
+
+(use-package! ivy-yasnippet
+  :commands (ivy-yasnippet))
+
+;; for navigation in epub files
+(use-package! ivy-pages
+  :commands ivy-pages)
+
+(use-package! js-react-redux-yasnippets
+  )
+
+(use-package! link-hint
+  :commands (link-hint-open-all-links
+             link-hint-copy-all-links
+             link-hint-open-link
+             link-hint-copy-link)
+  :config
+  (setq link-hint-avy-all-windows nil)
+  )
+
+(use-package! ob-javascript
+  :after ob-core
+  :config
+  (advice-add #'ob-javascript--node-path :override #'aj/ob-javascript--node-path))
+
+(use-package! org-brain
+  ;; :after org
+  :commands
+  (org-brain-visualize
+   org-brain-add-parent
+   org-brain-add-child
+   org-brain-add-friendship
+   org-brain-add-relationship
+   org-brain-add-resource
+   org-brain-goto-parent
+   org-brain-goto-child
+   org-brain-goto-friend
+   org-brain-goto-current
+   org-brain-goto-end
+   org-brain-goto-other-window
+   org-brain-remove-child
+   org-brain-remove-friendship
+   org-brain-remove-parent
+   )
+  :init
+  (add-to-list 'evil-motion-state-modes 'org-brain-visualize-mode)
+  :config
+  (add-hook 'org-brain-visualize-mode-hook #'visual-line-mode)
+  (advice-add #'org-brain-visualize :after #'aj/take-care-of-org-buffers)
+  (advice-add #'org-brain-entry-at-pt :override #'aj/org-brain-entry-at-pt)
+  (setq org-brain-visualize-default-choices 'all
+        org-brain-title-max-length -1
+        org-brain-path +TECHNICAL
+        org-brain-data-file (expand-file-name ".org-brain-data.el" (concat user-emacs-directory ".local/cache"))
+        org-brain-include-file-entries t
+        org-brain-file-entries-use-title t
+        )
+  )
+
+(use-package! org-ebook
+  :commands (org-ebook-open org-ebook-store-link)
+  )
+
+(use-package! org-pdfview
+  :commands (org-pdfview-open org-pdfview-store-link org-pdfview-complete-link org-pdfview-export)
+  )
+
+(use-package! org-ql
+  :after org
+  :commands org-ql-search
+  :config
+  (advice-add #'org-ql--select :around #'doom-shut-up-a)
+  (advice-add #'org-ql-view-refresh :around #'doom-shut-up-a)
+  (advice-add #'org-ql-view-refresh :after (lambda (&rest _) (org-agenda-filter-apply aj/agenda-filter 'tag)))
+  (advice-add #'org-ql-view--format-element :override #'aj/org-ql-view--format-element)
+  )
+
+(use-package! org-sidebar
+  :commands (org-sidebar org-sidebar-tree org-sidebar-ql)
+  )
+
+(use-package! org-super-agenda
+  :after org
+  :config
+  (org-super-agenda-mode)
+  (setq org-super-agenda-header-map (make-sparse-keymap))
+  )
+
+(use-package! powerthesaurus
+  :commands (powerthesaurus-lookup-word
+             powerthesaurus-lookup-word-dwim
+             powerthesaurus-lookup-word-at-point))
+
+(use-package! robots-txt-mode
+  :mode (("/robots\\.txt\\'" . robots-txt-mode)))
+
+(use-package! sdcv
+  :commands (sdcv-search-input sdcv-search-pointer)
+  :config
+  (set-popup-rule! "*SDCV\*"                    :size 0.4  :side 'top :select t))
+
+(use-package! systemd
+  :commands (systemd-mode))
+
+;; needed for ereader
+(use-package! xml+
+  :commands (xml+-query--generic xml+-query-all xml+-query-first xml+-node-text xml+-node-text--helper))
+
+(use-package! x-path-walker
+  :commands (helm-x-path-walker  x-path-get-mode))
+
+(use-package! vimrc-mode
+  :commands vimrc-mode
+  )
+
+(use-package! yankpad
+  :commands
+  (yankpad-append-category
+   yankpad-capture-snippet
+   yankpad-edit
+   yankpad-expand
+   yankpad-insert
+   yankpad-map
+   yankpad-reload
+   yankpad-repeat
+   yankpad-set-category)
+  :config
+  (setq yankpad-file (expand-file-name "yankpad.org" org-directory))
+  )
+
+(use-package! zeal-at-point
+  :commands (zeal-at-point zeal-at-point-search zeal-at-point-set-docset)
+  :config
+  (add-to-list 'zeal-at-point-mode-alist '(web-mode . "html"))
+  (add-to-list 'zeal-at-point-mode-alist '(pug-mode . ("html" "pug")))
+  )
 
 (toggle-frame-maximized)
 
