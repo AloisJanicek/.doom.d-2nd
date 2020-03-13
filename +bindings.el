210cb34e14b0b2af94b7119086e91166c3d7f910
Alois Janíček <janicek.dev@gmail.com>
2020-03-13 02:34:48 +0100

    fix wrong order of arguments

--- +bindings.el
+++ +bindings.el
@@ -577,8 +577,8 @@
                                               (interactive)
                                               (let ((hydra-hint-display-type 'message)) (aj/clocking/body)))
         (:prefix ("C" . "calibre")
-          :desc "technical"             "c" (lambda! (aj/open-calibre-book (expand-file-name +Libraries "Technical/")))
-          :desc "personal"              "p" (lambda! (aj/open-calibre-book (expand-file-name +Libraries "Personal/")))
+          :desc "technical"             "c" (lambda! (aj/open-calibre-book (expand-file-name "Technical/" +Libraries)))
+          :desc "personal"              "p" (lambda! (aj/open-calibre-book (expand-file-name "Personal/" +Libraries)))
           )
 
         :desc "agenda"                   "A" #'org-agenda
