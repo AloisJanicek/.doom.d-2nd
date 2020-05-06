6af4d2aabdc917c79f0004a22c6641b2461e8858
Alois Janíček <janicek.dev@gmail.com>
2020-05-06 19:23:18 +0200

    set org-src-lang-modes

--- config.el
+++ config.el
@@ -10,7 +10,25 @@
 (load! "+hacks")
 
 ;; fix void variables errors related to lazy (or explicit) loading
-(setq org-src-lang-modes nil
+(setq org-src-lang-modes
+      '(("arduino" . arduino)
+        ("redis" . redis)
+        ("php" . php)
+        ("md" . markdown)
+        ("C" . c)
+        ("C++" . c++)
+        ("asymptote" . asy)
+        ("bash" . sh)
+        ("beamer" . latex)
+        ("calc" . fundamental)
+        ("cpp" . c++)
+        ("ditaa" . artist)
+        ("dot" . fundamental)
+        ("elisp" . emacs-lisp)
+        ("ocaml" . tuareg)
+        ("screen" . shell-script)
+        ("shell" . sh)
+        ("sqlite" . sql))
       projectile-known-projects nil
       )
 
