aa9dfae3b89f516621067942298ccc1a8988bbb5
Alois Janíček <janicek.dev@gmail.com>
2020-05-06 19:35:39 +0200

    move code into more appropriate file

--- +hacks.el
+++ +hacks.el
@@ -1,5 +1,28 @@
 ;;; ~/.doom.d/+hacks.el -*- lexical-binding: t; -*-
 
+;; fix void variables errors related to lazy (or explicit) loading
+(setq org-src-lang-modes
+      '(("arduino" . arduino)
+        ("redis" . redis)
+        ("php" . php)
+        ("md" . markdown)
+        ("C" . c)
+        ("C++" . c++)
+        ("asymptote" . asy)
+        ("bash" . sh)
+        ("beamer" . latex)
+        ("calc" . fundamental)
+        ("cpp" . c++)
+        ("ditaa" . artist)
+        ("dot" . fundamental)
+        ("elisp" . emacs-lisp)
+        ("ocaml" . tuareg)
+        ("screen" . shell-script)
+        ("shell" . sh)
+        ("sqlite" . sql))
+      projectile-known-projects nil
+      )
+
 (after! org-clock
   (advice-add #'org-dblock-write:clocktable
               :override
--- config.el
+++ config.el
@@ -9,29 +9,6 @@
 (load! "+vars")
 (load! "+hacks")
 
-;; fix void variables errors related to lazy (or explicit) loading
-(setq org-src-lang-modes
-      '(("arduino" . arduino)
-        ("redis" . redis)
-        ("php" . php)
-        ("md" . markdown)
-        ("C" . c)
-        ("C++" . c++)
-        ("asymptote" . asy)
-        ("bash" . sh)
-        ("beamer" . latex)
-        ("calc" . fundamental)
-        ("cpp" . c++)
-        ("ditaa" . artist)
-        ("dot" . fundamental)
-        ("elisp" . emacs-lisp)
-        ("ocaml" . tuareg)
-        ("screen" . shell-script)
-        ("shell" . sh)
-        ("sqlite" . sql))
-      projectile-known-projects nil
-      )
-
 (add-to-list 'org-modules 'ol-info)
 (add-to-list 'org-modules 'ol-eww)
 
