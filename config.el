3f0801f6598e8ee042c312937743da72ae99a9c2
Alois Janíček <janicek.dev@gmail.com>
2021-09-19 22:10:28 +0200

    autoload org-roam-ui

--- config.el
+++ config.el
@@ -1910,13 +1910,7 @@ When in org-roam file, also create top-level ID.
   :after org-roam)
 
 (use-package! org-roam-ui
-  :after org-roam ;; or :after org
-
-  ;; normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-  ;; a hookable mode anymore, you're advised to pick something yourself
-  ;; if you don't care about startup time, use
-
-  ;; :hook (after-init . org-roam-ui-mode)
+  :commands (org-roam-ui-mode)
   :config
   (setq org-roam-ui-sync-theme t
         org-roam-ui-follow t
