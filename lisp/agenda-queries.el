ecd3da6176a6b74a56c570466626926d7da81f25
Alois Janíček <janicek.dev@gmail.com>
2021-11-12 02:50:42 +0100

    hide completed items from scheduled views

--- lisp/agenda-queries.el
+++ lisp/agenda-queries.el
@@ -157,6 +157,7 @@ to resole their precedence.
   "Return valid org-ql query searching for future dues."
   (let ((up-to (if current-prefix-arg 365 2)))
     `(or (and (planning :from 1 :to ,up-to))
+         (and (not '(done)))
          (and (habit)
               (planning :to ,up-to)
               (not (habit-half-due))))))
